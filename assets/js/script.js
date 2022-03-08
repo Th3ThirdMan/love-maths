520cefa9385421296480b8aa4dc27bb3db7a6047
Th3ThirdMan <davidgkennedymail@gmail.com>
2022-03-08 17:26:39 +0000

    Add Code for Subtraction

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,7 +33,9 @@ function runGame(gameType) {
         displayAdditionQuestion(num1, num2);
     } else if (gameType === "multiply") {
         displayMultiplyQuestion(num1, num2);
-    } else {
+    } else if(gameType === "subtract") {
+        displaySubtractQuestion(num1, num2);
+    }else{
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
     }
@@ -76,7 +78,9 @@ function calculateCorrectAnswer() {
         return [operand1 + operand2, "addition"];
     } else if (operator === "x") {
         return [operand1 * operand2, "multiply"];
-    } else {
+    } else if (operator === "-") {
+        return [operand1 - operand2, "subtract"];
+    }else{
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
     }
@@ -112,7 +116,9 @@ function displayAdditionQuestion(operand1, operand2) {
 }
 
 function displaySubtractQuestion() {
-
+    document.getElementById('operand1').textContext = operand1 > operand2 ? operand1:operand2;
+    document.getElementById('operand2').textContext = operand1 > operand2 ? operand1:operand2;
+    document.getElementById('operator').textContent = "-";
 }
 
 function displayMultiplyQuestion(operand1, operand2) {
